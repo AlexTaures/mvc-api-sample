51a6668e5c3e5c7b1cde8ae65569b8ed8ad4ef0f
oscaralexander.taures <oscaralexander.taures@salud.gob.sv>
2023-11-23 14:45:48 -0600

    adding user entity

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # mvc-api-sample
 Symfony 6 based Template
+
+#create entity:
+php bin/console make:entity
+
+#make migration:
+php bin/console make:migration
+
+#exec migration: #Need to create database backup
+php bin/console doctrine:migrations:migrate
+
+#drop a remake migration:
+php bin/console doctrine:migrations:migrate
+
---
+++ src/Entity/User.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+#[ORM\Table(name: '`user`')]
+class User
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $password = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $creation_date = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): static
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function getCreationDate(): ?\DateTimeInterface
+    {
+        return $this->creation_date;
+    }
+
+    public function setCreationDate(\DateTimeInterface $creation_date): static
+    {
+        $this->creation_date = $creation_date;
+
+        return $this;
+    }
+}
